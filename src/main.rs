8bcf94adc1a63a86ff79b73999bffbda05afc299
theawakener0 <wakeupwithme000@gmail.com>
2026-08-19 10:52:20 +0300

    ADDED dataset()...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+use rand::seq::SliceRandom;
+use rand::rng;
+
+fn dataset() -> Vec<String> {
+    let mut dataset = Vec::new();
+
+    let file_contents = fs::read_to_string("dataset/input.txt").expect("Couldn't read the file");
+
+    for line in file_contents.lines() {
+        dataset.push(line.to_string());
+    }
+
+    let mut rng = rng();
+    dataset.shuffle(&mut rng);
+
+    dataset
+}
+
 fn main() {
-    println!("Hello, world!");
+    let dataset = dataset();
+    println!("{:#?}", dataset);
 }
